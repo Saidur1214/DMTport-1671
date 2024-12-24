e4b1cb94792de4e1a7354540e3bb2c914c1cc416
Saidur1170 <112820365+Saidur1214@users.noreply.github.com>
2024-12-24 23:19:01 +0600

    Update script.js

---
+++ script.js
@@ -0,0 +1,57 @@
+console.log("Prooceding Javascript");
+
+let web_name = document.getElementById("web-name");
+let web_url = document.getElementById("web-url");
+let push_btn = document.getElementById("web-push");
+let placer = document.querySelector(".placer");
+
+function loadcard() {
+  if (localStorage.length === 0) {
+    console.log("Failed to load load or no data INIT");
+    return;
+  }
+
+  for (let l = 0; l < localStorage.length; l++) {
+    let stored_name = localStorage.key(l);
+    let stored_url = localStorage.getItem(stored_name);
+
+    let create_card = document.createElement("div");
+
+    create_card.classList.add("card");
+
+    create_card.innerHTML = `<span class="w-name">${stored_name}</span><span class="w-url">${stored_url}</span>`;
+
+    placer.append(create_card);
+  }
+}
+
+loadcard();
+
+function inject_sites() {
+  let web_name_value = web_name.value;
+  let web_url_value = web_url.value;
+
+  if (!web_name_value || !web_url_value) {
+    alert("error!");
+    return;
+  }
+
+  let create_card = document.createElement("div");
+
+  create_card.classList.add("card");
+
+  create_card.innerHTML = `<span class="w-name">${web_name_value}</span><span class="w-url">${web_url_value}</span>`;
+
+  placer.append(create_card);
+
+  localStorage.setItem(web_name_value, web_url_value);
+}
+
+push_btn.addEventListener("click", inject_sites);
+
+function storecard(web_name, web_url) {
+  let web_name_value = web_name.value;
+  let web_url_value = web_url.value;
+
+  let storage = [];
+}
